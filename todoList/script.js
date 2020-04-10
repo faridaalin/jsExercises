afd93f32e5ea18fdcd6523e690d25afa0c64cd55
faridaalin <56302698+faridaalin@users.noreply.github.com>
2020-04-11 00:06:50 +0200

    js update

--- todoList/script.js
+++ todoList/script.js
@@ -7,76 +7,47 @@ const inputField = document.querySelector("#inputField");
 
 const listContainer = document.querySelector("#list");
 
-
 // Add item to the list
 const addBtn = document.querySelector("#addBtn");
 
 addBtn.addEventListener("click", (event) => {
-  todos.push(inputField.value)
-  //todos.push(inputField.value);
-  addTodos() 
+  if (inputField.value) {
+    todos.push(inputField.value);
+    addTodos(todos);
+  } else {
+    alert("add something to your list");
+  }
 });
 
- inputField.addEventListener('keyup', (event)=> {
-   
-    if(event.keyCode === 13) {
-  todos.push(inputField.value);
-       addTodos(inputField.value) 
-     }
- });
+inputField.addEventListener("keyup", (event) => {
+  if (event.keyCode === 13 && inputField.value) {
+    todos.push(inputField.value);
+    addTodos(todos);
+  }
+});
 
-const addTodos = (inputValue) => {
+const addTodos = (todos) => {
   listContainer.innerHTML = "";
 
-  if (inputField.value) {
-    
-    for(const todo of todos) {
-      listContainer.innerHTML += `<div class="listContainer">
+  todos.forEach((todo, index) => {
+    listContainer.innerHTML += `<div class="listContainer">
       <input type="checkbox" name="" class="inpToggle" id="inpToggle">
-      <label for="">${todo}<i class="fa fa-trash ml-8" id="delete"></i></label>
+      <label for="">${todo}<i class="fa fa-trash ml-8" id="delete" data-index="${index}"></i></label>
       </div>`;
-    };
-
     inputField.value = "";
-  } else {
-    inputField.value = "";
-  }
- 
-  removeItem()
- 
+  });
+  removeItem();
 };
 
-
-
-
-// Remove item from the list
+// Remove item from the list and update the list and display it
 const removeItem = () => {
+  const deleteIcon = document.querySelectorAll("#delete");
 
-  for(const todo of todos) {}
-    const removeItem = document.querySelectorAll('#delete')
-    removeItem.forEach((item) => {
-      item.addEventListener('click', () => {
-        item.parentNode.parentElement.remove()
-
-      })
+  for (let j = 0; j < deleteIcon.length; j++) {
+    let index = deleteIcon[j].dataset.index;
+    deleteIcon[j].addEventListener("click", () => {
+      todos.splice(index, 1);
+      addTodos(todos);
     });
-
-}
-
-// const toggleTodo = ()=> {
-//   const toggle = document.querySelector("#inpToggle");
-
-//   for(const todo of todos) {
-//     console.log(todo.checked === true)
-//     if(toggle.checked === true) {
-//       console.log('chould change');
-      
-//       listContainer.style.backgroundColor = "red";
-//     } else {
-//       console.log('nothing')
-//     }
-//   }
-  
-// }
-
-
+  }
+};
