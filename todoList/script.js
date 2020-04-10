2bbaa7f214a60301a3159fc17e6451427eb2c1cb
faridaalin <56302698+faridaalin@users.noreply.github.com>
2020-04-10 11:59:17 +0200

    HTML and CSS

---
+++ todoList/script.js
@@ -0,0 +1,11 @@
+// able to aff to the Todo list
+// Able to delete from the todo list
+// Able to toggle to set text-decoration: line-through
+
+
+
+const inputField = document.querySelector('#inputField');
+console.dir(inputField)
+const addBtn = document.querySelector('#addBtn')
+const toggle = document.querySelector('#toggle')
+const delete = document.querySelector('#delete')
