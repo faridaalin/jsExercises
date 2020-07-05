48c527bd977ac8a88d84ebc53e008a94de2e9d34
faridaalin <56302698+faridaalin@users.noreply.github.com>
2020-07-05 14:49:10 +0200

    missing files

--- crwn-clothing/src/pages/shop/shop.component.jsx
+++ crwn-clothing/src/pages/shop/shop.component.jsx
@@ -26,26 +26,3 @@ class ShopPage extends React.Component {
 }
 
 export default ShopPage;
-
-// class ShopPage extends React.Component {
-//     constructor(props) {
-//         super(props);
-
-//         this.state = {
-//             collections: SHOP_DATA
-//         };
-//     }
-
-//     render() {
-//         const { collections } = this.state;
-//         return (
-//             <div className='shop-page'>
-//                 {collections.map(({ id, ...otherCollectionProps }) => (
-//                     <CollectionPreview key={id} {...otherCollectionProps} />
-//                 ))}
-//             </div>
-//         );
-//     }
-// }
-
-// export default ShopPage;
