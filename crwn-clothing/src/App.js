84ae5a6758d00dd4d9981af50fb9e913db69070d
faridaalin <56302698+faridaalin@users.noreply.github.com>
2020-07-05 14:16:45 +0200

    updated app with shop page, collection prev and collection item comp

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import './App.css';
 import { Route, Switch } from 'react-router-dom';
-import HomePage from './pages/homepage/homepage.component'
 
-const HatsPage = () => (
-  <div>
-    <h1> HATS PAGE</h1>
-  </div>
-);
+import './App.css';
+
+import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component.jsx'
 
 function App() {
   return (
     <div>
       <Switch>
       <Route exact path='/' component={HomePage} />
-      <Route exact path='/hats' component={HatsPage} />
-      {/* <HomePage /> */}
+      <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
