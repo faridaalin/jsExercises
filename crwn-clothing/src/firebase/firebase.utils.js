83b7585fbf2139897970e8048c5e2f6b37e4805e
faridaalin <56302698+faridaalin@users.noreply.github.com>
2020-07-06 11:47:28 +0200

    implemented firebas utils

--- crwn-clothing/src/firebase/firebase.utils.js
+++ crwn-clothing/src/firebase/firebase.utils.js
@@ -11,7 +11,36 @@ const config = {
     messagingSenderId: "939232259241",
     appId: "1:939232259241:web:5c3bad3bba334904988db8"
   };
-firebase.initializeApp(config);
+
+  firebase.initializeApp(config);
+
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+  
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+  
+    const snapShot = await userRef.get();
+  
+    if (!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        });
+      } catch (error) {
+        console.log('error creating user', error.message);
+      }
+    }
+  
+    return userRef;
+  };
+
+
+
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
