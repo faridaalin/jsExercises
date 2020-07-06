77fca872d89dd333376b893d83ac7f0569c6a8c2
faridaalin <56302698+faridaalin@users.noreply.github.com>
2020-07-06 11:59:05 +0200

    sign in handleSubmit method

--- crwn-clothing/src/components/header/header.component.jsx
+++ crwn-clothing/src/components/header/header.component.jsx
@@ -7,26 +7,29 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const Header = ({ currentuser }) => (
-    <div className='header'>
-        <Link className='logo-container' to='/'>
-            <Logo className='logo' />
-        </Link>
-        <div className="options">
-        <Link className="option" to='/shop'>
-            SHOP
-        </Link>
-        <Link className="option" to='/shop'>
-            CONTACT
-        </Link>
-        {
-            currentuser ? 
-            <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-            :
-            <Link className='option' to='/signin'>SIGN IN</Link>
-        }
+const Header = ({ currentUser }) => (
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
+    </Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>
+        SHOP
+      </Link>
+      <Link className='option' to='/shop'>
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
+          SIGN OUT
         </div>
+      ) : (
+        <Link className='option' to='/signin'>
+          SIGN IN
+        </Link>
+      )}
     </div>
-)
+  </div>
+);
 
-export default Header
+export default Header;
